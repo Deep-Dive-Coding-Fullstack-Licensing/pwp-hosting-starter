17c85cf2852c11f57264746ed6059556097d37e7
GKephart <gkephart@cnm.edu>
2020-05-30 19:28:54 -0600

    Recaptcha error handling is working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-require('dotenv').config()
-const express = require("express")
+const express = require('express')
 const bodyParser = require('body-parser')
-const morgan = require("morgan")
-const mailgun = require("mailgun-js")
+const morgan = require('morgan')
+const mailgun = require('mailgun-js')
 const {check, validationResult} = require('express-validator');
+const Recaptcha = require('express-recaptcha').RecaptchaV2
 
 const app = express()
+console.log(Recaptcha)
+console.log(process.env)
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
 
 //app.use allows for different middleware to be brought into Express
 app.use(morgan("dev"))
@@ -31,9 +34,13 @@ const validationArray = [
 //how to configure a route for express
 indexRoute.route("/apis")
   .get(indexRouteMiddleware)
-  .post(validationArray, (request, response) => {
+  .post(validationArray, recaptcha.middleware.verify,(request, response) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(request);
+
+    if(request.recaptcha.error) {
+      console.log(request.recaptcha.error)
+    }
 
     if (!errors.isEmpty()) {
       const currentError = errors.array()[0]
@@ -62,6 +69,8 @@ indexRoute.route("/apis")
         ))
       }
     })
+
+
 
     return response.send(Buffer.from("<div class='alert alert-success' role='alert'>Email successfully sent.</div>"))
 
